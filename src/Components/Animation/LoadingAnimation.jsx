2cfc6ba989a331ad0d9796b7e5276fa52041ffa9
tanvir <tanvir0046@gmail.com>
2024-07-27 23:41:26 +0600

    make a loading page for the websit

--- src/Components/Animation/LoadingAnimation.jsx
+++ src/Components/Animation/LoadingAnimation.jsx
@@ -2,13 +2,14 @@
 
 const LoadingAnimation = () => {
     return (
-        <div className="fixed top-0 left-0 w-full h-full z-50 flex justify-center items-center">
+        <div className="fixed top-0 left-0 w-full h-full z-50 flex flex-col justify-center items-center">
             <iframe
                 className="w-[500px] h-[500px] sm:w-[500px] sm:h-[500px] md:w-[700px] md:h-[700px] lg:w-[800px] lg:h-[800px]"
                 src="https://lottie.host/embed/1ebda872-c417-4307-b7fe-0042bc733a01/u4CxUY1pt6.json"
                 title="Loading Animation"
                 allowFullScreen
             ></iframe>
+            <h1 className="text-6xl text-[#FF5700] h1-text tracking-wider">Sip Bite</h1>
         </div>
     );
 };
