63563ffcf26a58073e8db7cb343e42df5361885c
tanvir <tanvir0046@gmail.com>
2024-08-03 18:50:05 +0600

    some section is change

--- src/Pages/Menu/Menupage.jsx
+++ src/Pages/Menu/Menupage.jsx
@@ -2,9 +2,9 @@ import { Fade } from "react-awesome-reveal";
 
 const Menupage = () => {
     return (
-        <div className="flex justify-center items-center">
+        <div className="flex justify-start items-start">
             <Fade direction='right' delay={20}>
-                <div className="text-center px-4 py-6 md:py-12 lg:py-16">
+                <div className="px-4 py-6 md:py-12 lg:py-16">
                     <h1 className="text-2xl md:text-4xl lg:text-7xl font-bold mb-4 text-[#FDF6D2]">
                         Welcome to Our Menu
                     </h1>
