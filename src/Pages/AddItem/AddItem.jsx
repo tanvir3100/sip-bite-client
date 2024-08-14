09c905fa387581ea49019367de3aeef308aff4f7
tanvir <tanvir0046@gmail.com>
2024-08-15 02:03:41 +0600

    make add item section

--- src/Pages/AddItem/AddItem.jsx
+++ src/Pages/AddItem/AddItem.jsx
@@ -1,11 +1,118 @@
+import { useForm } from 'react-hook-form';
+import Swal from 'sweetalert2';
+import useAxios from '../../Hooks/useAxios';
 
+const AddItems = () => {
+    const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+    const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
+    const { register, handleSubmit, reset } = useForm();
+    const axiosInstance = useAxios();
 
-const AddItem = () => {
+    const onSubmit = async (data) => {
+        try {
+            console.log(data);
+
+            // Image upload to imgbb
+            const formData = new FormData();
+            formData.append('image', data.image[0]);
+
+            const res = await axiosInstance.post(image_hosting_api, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+
+            if (res.data.success) {
+                // Prepare product data
+                const productItem = {
+                    name: data.name,
+                    image: res.data.data.display_url,
+                    price: parseFloat(data.price),
+                    category: data.category,
+                    description: data.description,
+                };
+
+                // Send product data to the server
+                const productRes = await axiosInstance.post('http://localhost:4900/products', productItem);
+                console.log(productRes.data);
+
+                if (productRes.data.insertedId) {
+                    reset();
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: `${data.name} added successfully`,
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
+            }
+        } catch (error) {
+            console.error('Error adding item:', error);
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Something went wrong! Please try again.',
+            });
+        }
+    };
+
     return (
         <div>
-            <h1 className="text-center text-5xl">This is our Add item section</h1>
+            <h1 className='text-5xl text-center'>Add Item</h1>
+            <h3 className='text-sm text-center'>Add your New Products</h3>
+            <div className="w-5/6 bg-slate-200 rounded-3xl p-5 mx-auto">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <label className="form-control w-full">
+                        <div className="label">
+                            <span className="label-text">Product Name*</span>
+                        </div>
+                        <input
+                            {...register("name", { required: true })}
+                            type="text"
+                            placeholder="Product Name"
+                            className="input input-bordered w-full" />
+                    </label>
+                    <div className="flex gap-5">
+                        <div className="flex-1">
+                            <label>
+                                <div className="label">
+                                    <span className="label-text">Select Category*</span>
+                                </div>
+                                <select defaultValue={'default'} {...register("category", { required: true })} className="select select-bordered w-full">
+                                    <option disabled value={'default'}>Select A Category</option>
+                                    <option value="Burger">Burger</option>
+                                    <option value="Pizza">Pizza</option>
+                                    <option value="Drinks">Drinks</option>
+                                </select>
+                            </label>
+                        </div>
+                        <div className="flex-1">
+                            <label className="form-control w-full">
+                                <div className="label">
+                                    <span className="label-text">Price*</span>
+                                </div>
+                                <input
+                                    {...register("price", { required: true })}
+                                    type="text"
+                                    placeholder="Price"
+                                    className="input input-bordered w-full" />
+                            </label>
+                        </div>
+                    </div>
+                    <label className="form-control">
+                        <div className="label">
+                            <span className="label-text">Recipe Details*</span>
+                        </div>
+                        <textarea {...register("description", { required: true })} className="textarea textarea-bordered h-24" placeholder="Recipe Details"></textarea>
+                    </label>
+                    <input type="file" {...register("image", { required: true })} className="file-input file-input-bordered w-full mt-5 max-w-xs" />
+                    <br />
+                    <button className="btn btn-outline mt-5">Add Item</button>
+                </form>
+            </div>
         </div>
     );
 };
 
-export default AddItem;
+export default AddItems;
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,9 +1,8 @@
 import { useContext } from 'react';
 import { AuthContext } from '../../Provider/AuthProvider/AuthProvider';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
 import HelmetSection from '../../Hooks/Helmet/HelmetSection';
-
 
 const Login = () => {
     const { signInUser } = useContext(AuthContext);
@@ -17,32 +16,36 @@ const Login = () => {
         const form = e.target;
         const email = form.email.value;
         const password = form.password.value;
-        console.log(email, password);
 
         signInUser(email, password)
             .then((result) => {
                 const user = result.user;
                 console.log(user);
-                Swal.fire("Log in Successful!");
+                Swal.fire("Log in Successful!", "You are now logged in as an admin.", "success");
                 navigate(from, { replace: true });
             })
             .catch((error) => {
                 console.error(error);
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Login Failed',
+                    text: 'Incorrect email or password. Please try again.',
+                });
             });
     };
 
     return (
         <div className="w-full min-h-screen flex justify-center items-center">
             <HelmetSection title={'Sip Bite | Log In'} />
-            <div className=" w-[80%] h-[80%] shadow-2xl mb-10 mt-10" >
+            <div className=" w-[50%] h-[80%] shadow-2xl mb-10 mt-10" >
                 <div className="hero-content flex-col lg:flex-row p-0">
                     <div className="text-center lg:text-left">
-
+                        {/* Additional content if needed */}
                     </div>
                     <div className="card shrink-0 w-full max-w-sm">
                         <form onSubmit={handleLogin} className="card-body">
                             <div className="form-control">
-                                <h1 className="text-2xl font-bold text-center">Login now!</h1>
+                                <h1 className="text-2xl font-bold text-center">Admin Log In Only!</h1>
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
@@ -53,15 +56,11 @@ const Login = () => {
                                     <span className="label-text">Password</span>
                                 </label>
                                 <input type="password" name='password' placeholder="password" className="input input-bordered" required />
-                                <label className="label">
-                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                                </label>
                             </div>
                             <div className="form-control">
-                                <input className='btn bg-[#D1A054B3] text-white' type="submit" value="Login" />
+                                <input className='btn bg-[#FF5700] text-[#FDF6D2]' type="submit" value="Login" />
                             </div>
                         </form>
-                        <p className='text-center mb-5'>Have no Account? <Link to='/register'><span className='hover:text-red-700 hover:underline'>Sign Up</span></Link></p>
                     </div>
                 </div>
             </div>
