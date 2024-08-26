d9fb3adb2259abd034021cd5cde08f02c2e91596
tanvir <tanvir0046@gmail.com>
2024-08-27 01:10:33 +0600

    all the section is now clean

--- src/Components/Animation/LoadingAnimation.jsx
+++ src/Components/Animation/LoadingAnimation.jsx
@@ -2,7 +2,7 @@
 
 const LoadingAnimation = () => {
     return (
-        <div className="fixed top-0 left-0 w-full h-full z-50 flex flex-col justify-center items-center">
+        <div className="fixed top-0 left-0 w-full min-h-screen z-50 flex flex-col justify-center items-center">
             <iframe
                 className="w-[500px] h-[500px] sm:w-[500px] sm:h-[500px] md:w-[700px] md:h-[700px] lg:w-[800px] lg:h-[800px]"
                 src="https://lottie.host/embed/c852c96f-5864-497f-a96b-e950e74d86f8/vQ9KdqF9Ix.json"
