cfa33ee1f6cdf29ae858b64e80d4ac2bb9670c99
tanvir <tanvir0046@gmail.com>
2024-08-27 00:37:29 +0600

    some problem is solved

--- src/Hooks/useAxios.jsx
+++ src/Hooks/useAxios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosPublic = axios.create({
-    baseURL: 'https://sipbite-server-side.vercel.app'
+    baseURL: 'https://sip-bite-server.vercel.app'
 })
 
 const useAxios = () => {
